5a70ea40969158179d60f67fd69f0e75462be474
Chris Hewitt <chris.hewitt@precedent.co.uk>
2012-10-07 23:02:12 +0100

    Changed ref to ASP.Net 4.0

--- contact-app/Models/CoverDetailsModel.cs
+++ contact-app/Models/CoverDetailsModel.cs
@@ -9,16 +9,16 @@ namespace contact_app.Models
     public sealed class CoverDetailsModel
     {
         [Required()]
-        [Display(Name="Please attach an up to date CV")]
+        [Display(Name = "Please attach an up to date CV")]
         public HttpPostedFileBase CurriculumVitae { get; set; }
 
         [Required()]
-        [MinLength(20)]
+        [StringLength(255, MinimumLength = 20)]
         [Display(Name = "What makes you suitable for the job?")]
         public string ReasonForAcceptance { get; set; }
 
         [Required()]
-        [MinLength(20)]
+        [StringLength(255, MinimumLength = 20)]
         [Display(Name = "Why do you want the job?")]
         public string ReasonForApplying { get; set; }
     }
